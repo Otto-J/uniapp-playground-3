2be7f79ad6a844fe0969caab111df2c8b6647139
jixinbao <jixinbao@dcloud.io>
2023-12-20 10:53:50 +0800

    Add pugPlugin to vite.config.ts and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
-
+import pugPlugin from "vite-plugin-pug";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [uni(), pugPlugin()],
 });
