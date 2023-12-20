b78599b32239db9adc8ccf48958ce4d55a254072
jixinbao <jixinbao@dcloud.io>
2023-12-20 10:57:30 +0800

    Add click event to div elements in index.vue

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,16 @@
 <template lang="pug">
 div
   // #ifdef MP-WEIXIN
-  div weixin
+  div(@click="sayHi") weixin
   // #endif
   // #ifdef H5
-  div h5 web
+  div(@click="sayHi") h5 web
   // #endif
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const sayHi = () => {
+  alert("alert");
+};
+</script>
 
 <style></style>
