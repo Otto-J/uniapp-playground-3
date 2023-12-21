c1e92303b59acaeca845dee67d2f5c4a042e8cc3
jixinbao <jixinbao@dcloud.io>
2023-12-21 16:44:53 +0800

    演示基础组件引入方式

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,12 +4,20 @@
     <view class="text-area">
       <text class="title">{{ title }}</text>
     </view>
+    <uni-card
+      title="基础卡片"
+      sub-title="副标题"
+      extra="额外信息"
+      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+    >
+      <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
+    </uni-card>
   </view>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+import { ref } from "vue";
+const title = ref("Hello");
 </script>
 
 <style>
