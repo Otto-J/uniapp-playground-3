fd637d338a8daf6c1bb3c39cabeb45ae07b3cc53
jixinbao <jixinbao@dcloud.io>
2023-12-22 10:28:43 +0800

    wip: add emit on

---
+++ src/pages/about/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>about page</div>
+</template>
+<script lang="ts" setup>
+import { onLoad, onUnload } from "@dcloudio/uni-app";
+uni.$on("update", function (data) {
+  console.log("监听到事件来自 update ，携带参数 msg 为：" + data.msg);
+});
+
+onLoad(() => {});
+</script>
+
+<style></style>
