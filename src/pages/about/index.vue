cc9028fe40d63984b9c9ae842e0608c22a68d000
jixinbao <jixinbao@dcloud.io>
2023-12-22 16:44:35 +0800

    Add message display in about page

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -1,14 +1,22 @@
 <template>
   <div>about page</div>
+  <div>
+    收到的信息：
+    {{ message }}
+  </div>
 </template>
 <script lang="ts" setup>
 import { onLoad, onUnload } from "@dcloudio/uni-app";
+import { ref } from "vue";
 
 // 思路是先发注册监听、再告诉上一个页面准备好了
+
+const message = ref("");
 
 onLoad(() => {
   uni.$on("update", function (data) {
     console.log("page2 监听到事件来自 update ，携带参数 msg 为：" + data.msg);
+    message.value = data.msg;
   });
   uni.$emit("page2Ready", { msg: "page2 准备好了，你发送吧" });
 });
