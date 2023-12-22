3ddd2704f167ba4f05324a6f507d5abd0826ceaa
jixinbao <jixinbao@dcloud.io>
2023-12-22 16:23:24 +0800

    Add event listeners and cleanup in about and index pages

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -2,15 +2,19 @@
   <div>about page</div>
 </template>
 <script lang="ts" setup>
-import { onLoad } from "@dcloudio/uni-app";
+import { onLoad, onUnload } from "@dcloudio/uni-app";
 
 // 思路是先发注册监听、再告诉上一个页面准备好了
-uni.$on("update", function (data) {
-  console.log("监听到事件来自 update ，携带参数 msg 为：" + data.msg);
-});
-uni.$emit("page2Ready", { msg: "page2 准备好了，你发送吧" });
 
-onLoad(() => {});
+onLoad(() => {
+  uni.$on("update", function (data) {
+    console.log("监听到事件来自 update ，携带参数 msg 为：" + data.msg);
+  });
+  uni.$emit("page2Ready", { msg: "page2 准备好了，你发送吧" });
+});
+onUnload(() => {
+  uni.$off("update");
+});
 </script>
 
 <style></style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -8,7 +8,7 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { nextTick } from "vue";
+import { onLoad, onUnload } from "@dcloudio/uni-app";
 
 // 这里演示，从 index 跳转到 about 时候，传递信息
 const go = () => {
@@ -17,9 +17,17 @@ const go = () => {
   });
 };
 
-uni.$on("page2Ready", function (data) {
-  console.log("收到 page2 发过来的信息了", data);
-  uni.$emit("update", { msg: "页面更新" });
+onLoad(() => {
+  // 思路是先发注册监听、再发送信息
+
+  uni.$on("page2Ready", function (data) {
+    console.log("收到 page2 发过来的信息了", data);
+    uni.$emit("update", { msg: "页面更新" });
+  });
+});
+
+onUnload(() => {
+  uni.$off("page2Ready");
 });
 </script>
 
