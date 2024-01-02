104ad5071af58e0319137fe9b92e8c3639c34cf4
jixinbao <jixinbao@dcloud.io>
2024-01-02 11:13:13 +0800

    wip: regexp

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,14 @@
 <template>
-  <div></div>
+  <div>div</div>
 </template>
 <script lang="ts" setup>
-const DEFAULT_STRIP_REGEXP = /[^\p{L}\d]+/giu;
-const DEFAULT_STRIP_REGEXP3 = new RegExp("[^\\p{L}\\d]+", "giu");
-console.log(DEFAULT_STRIP_REGEXP, DEFAULT_STRIP_REGEXP3);
+// const DEFAULT_STRIP_REGEXP = /[^\p{L}\d]+/giu;
+// const DEFAULT_STRIP_REGEXP3 = new RegExp("[^\\p{L}\\d]+", "giu");
+// console.log(
+//   // DEFAULT_STRIP_REGEXP,
+//   DEFAULT_STRIP_REGEXP3
+//   // DEFAULT_STRIP_REGEXP.toString() === DEFAULT_STRIP_REGEXP3.toString()
+// );
 </script>
 
 <style></style>
