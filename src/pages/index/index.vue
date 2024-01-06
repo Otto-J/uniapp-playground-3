7e4eff79ec3f7c3a23fab0353199945bd9687ece
jixinbao <jixinbao@dcloud.io>
2024-01-06 15:11:09 +0800

    Update index.vue with new image preview functionality

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,86 +1,33 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-    <uni-card
-      title="基础卡片"
-      sub-title="副标题"
-      extra="额外信息"
-      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-    >
-      <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
-    </uni-card>
-    <button @click="uploadImage">上传文件</button>
-  </view>
+  <div>
+    <h2>uni.chooseImage 推荐使用 uni.chooseMedia</h2>
+    <h2>uni.previewImage</h2>
+    <button @click="previewImag">预览图片</button>
+  </div>
 </template>
+<script lang="ts" setup>
+const previewImag = () => {
+  uni.previewImage({
+    current: 0,
+    urls: [
+      "https://1035353822.qnqcdn.net:22443/qn-XVnEBQiNsKSZLNhoiFj4uMVYtWjsUJSk2XG0aLw4.img.cdn.aliyun.dcloud.net.cn/guide/uniapp/gh_33446d7f7a26_430.jpg",
+      "https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/uni-app.png",
+    ],
+    // indicator: "default",
+    // loop: true,
 
-<script setup lang="ts">
-import { ref } from "vue";
-const title = ref("Hello");
-
-const uploadImage = () => {
-  uni.chooseVideo({
-    sourceType: ["camera", "album"],
-    success: (chooseImageRes) => {
-      const tempFilePaths = chooseImageRes.tempFilePath;
-      // debugger;
-      uni.uploadFile({
-        url: "http://192.168.31.60:3000/upload", //仅为示例，非真实的接口地址
-        filePath: tempFilePaths,
-        name: "file",
-        // only for alipay
-        hideLoading: true,
-
-        success: (uploadFileRes) => {
-          console.log(uploadFileRes.data);
-          // alert ok
-          uni.showToast({
-            title: "上传成功",
-            icon: "success",
-            duration: 2000,
-          });
-        },
-        fail: (err) => {
-          console.log(err);
-          // alert fail
-          uni.showToast({
-            title: "上传失败",
-            icon: "none",
-            duration: 2000,
-          });
-        },
-      });
+    showmenu: false,
+    success: function () {
+      console.log("success");
+    },
+    fail: function (err) {
+      console.log("fail:" + err.errMsg);
+    },
+    complete: function () {
+      console.log("complete");
     },
   });
 };
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
-</style>
+<style></style>
