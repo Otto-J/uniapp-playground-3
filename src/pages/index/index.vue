cd5578168bcf3a26b03883f7d7641d6cbed33cb9
jixinbao <jixinbao@dcloud.io>
2024-01-09 12:05:35 +0800

    wip

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,14 +4,8 @@
     <!-- <uni-button type="primary">hi button</uni-button> -->
     <!-- <uni-button>hi button</uni-button> -->
 
-    <uni-card
-      title="基础卡片"
-      sub-title="副标题"
-      extra="额外信息"
-      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-    >
-      <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
-    </uni-card>
+    <div>{{ title }}</div>
+
     <button @click="uploadImage">上传文件</button>
   </view>
 </template>
@@ -24,6 +18,12 @@ const title = ref("Hello");
 
 onLoad(() => {
   console.log("onLoad");
+  uni.getNetworkType({
+    success: function (res) {
+      console.log(res.networkType);
+      title.value = res.networkType;
+    },
+  });
   // alert(
   //   (navigator as any).connection ||
   //     (navigator as any).webkitConnection ||
