17112c1a796df5c463ec09f2cd6949068af286ba
jixinbao <jixinbao@dcloud.io>
2024-01-10 18:02:44 +0800

    feat: 演示 page-meta 的功能

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -1,31 +1,53 @@
 <template>
-  <div>
-    <div>about page</div>
-    <view class="ctrlItemContainer">
-      <uni-icons type="auth" size="30" class="tip-icon"></uni-icons>
-      <uni-easyinput v-model="username" class="uni-mt-5" trim="all" @focus="handleInputFocus" />
-    </view>
-    <view class="ctrlItemContainer">
-      <uni-icons type="locked" size="30" class="tip-icon"></uni-icons>
-      <uni-easyinput
-        v-model="password"
-        type="password"
-        class="uni-mt-5"
-        trim="all"
-        @focus="handleInputFocus"
-      />
-    </view>
-  </div>
+  <page-meta
+    :background-text-style="bgTextStyle"
+    :background-color="bgColor"
+    :background-color-top="bgColorTop"
+    :background-color-bottom="bgColorBottom"
+    :scroll-top="scrollTop"
+    page-style="color: green"
+    :enable-pull-down-refresh="true"
+    root-font-size="16px"
+  >
+    <navigation-bar
+      :title="nbTitle"
+      :loading="nbLoading"
+      :front-color="nbFrontColor"
+      :background-color="nbBackgroundColor"
+    />
+  </page-meta>
+  <view class="content">
+    <div>这是内容样式受到 page-meta 控制</div>
+
+    <navigator url="/pages/index/index" open-type="navigate" hover-class="navigator-hover">
+      <button>返回上一页</button>
+    </navigator>
+    <div style="height: 200vh"></div>
+    <div>buttom</div>
+  </view>
 </template>
-<script lang="ts" setup>
-import { ref } from "vue";
 
-const username = ref("");
-const password = ref("");
-
-const handleInputFocus = (e) => {
-  console.log(e);
+<script>
+export default {
+  data() {
+    return {
+      bgTextStyle: "dark",
+      scrollTop: "200rpx",
+      bgColor: "#ff0000",
+      bgColorTop: "#00ff00",
+      bgColorBottom: "#0000ff",
+      nbTitle: "任意标题",
+      nbLoading: false,
+      nbFrontColor: "blue",
+      nbBackgroundColor: "green",
+    };
+  },
+  onLoad() {
+    setInterval(() => {
+      this.nbTitle = new Date().toLocaleTimeString();
+      // this.nbLoading = !this.nbLoading;
+    }, 1000);
+  },
+  methods: {},
 };
 </script>
-
-<style></style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <div>hi</div>
+    <navigator url="/pages/about/index" open-type="navigate" hover-class="navigator-hover">
+      <button>去 about</button>
+    </navigator>
+    <input
+      type="text"
+      v-model="title"
+      :focus="focus"
+      ref="inputRef"
+      style="outline: 1px solid red"
+    />
   </div>
 </template>
 <script lang="ts" setup>
@@ -8,10 +18,20 @@ import {
   onNavigationBarSearchInputClicked,
   onNavigationBarSearchInputChanged,
   onNavigationBarSearchInputConfirmed,
+  onLoad,
 } from "@dcloudio/uni-app";
+import { onMounted, ref, nextTick } from "vue";
+const focus = ref(false);
+const title = ref("abc");
 
-onNavigationBarSearchInputClicked((e) => {
-  console.log("onNavigationBarSearchInputClicked", e);
+const inputRef = ref<HTMLInputElement>();
+
+setTimeout(() => {
+  focus.value = true;
+}, 4000);
+
+onNavigationBarSearchInputClicked(() => {
+  console.log("onNavigationBarSearchInputClicked");
 });
 
 onNavigationBarSearchInputConfirmed((e) => {
