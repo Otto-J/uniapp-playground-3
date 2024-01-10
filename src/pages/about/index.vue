ac233a11b61e049c5d33381201449c41ebc5aeaf
jixinbao <jixinbao@dcloud.io>
2024-01-10 17:30:28 +0800

    feat: 演示原生导航搜索使用方式

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>about page</div>
+  <div>
+    <div>about page</div>
+    <view class="ctrlItemContainer">
+      <uni-icons type="auth" size="30" class="tip-icon"></uni-icons>
+      <uni-easyinput v-model="username" class="uni-mt-5" trim="all" @focus="handleInputFocus" />
+    </view>
+    <view class="ctrlItemContainer">
+      <uni-icons type="locked" size="30" class="tip-icon"></uni-icons>
+      <uni-easyinput
+        v-model="password"
+        type="password"
+        class="uni-mt-5"
+        trim="all"
+        @focus="handleInputFocus"
+      />
+    </view>
+  </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const username = ref("");
+const password = ref("");
+
+const handleInputFocus = (e) => {
+  console.log(e);
+};
+</script>
 
 <style></style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,95 +1,26 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-    <uni-easyinput>
-      <template #right>
-        <view>密码</view>
-      </template>
-    </uni-easyinput>
-
-    <u-search-bar />
-
-    <uni-card
-      title="基础卡片"
-      sub-title="副标题"
-      extra="额外信息"
-      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-    >
-      <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
-    </uni-card>
-    <button @click="uploadImage">上传文件</button>
-  </view>
+  <div>
+    <div>hi</div>
+  </div>
 </template>
+<script lang="ts" setup>
+import {
+  onNavigationBarSearchInputClicked,
+  onNavigationBarSearchInputChanged,
+  onNavigationBarSearchInputConfirmed,
+} from "@dcloudio/uni-app";
 
-<script setup lang="ts">
-import { ref } from "vue";
-import uSearchBar from "./u-search-bar.vue";
-const title = ref("Hello");
+onNavigationBarSearchInputClicked((e) => {
+  console.log("onNavigationBarSearchInputClicked", e);
+});
 
-const uploadImage = () => {
-  uni.chooseVideo({
-    sourceType: ["camera", "album"],
-    success: (chooseImageRes) => {
-      const tempFilePaths = chooseImageRes.tempFilePath;
-      // debugger;
-      uni.uploadFile({
-        url: "http://192.168.31.60:3000/upload", //仅为示例，非真实的接口地址
-        filePath: tempFilePaths,
-        name: "file",
-        // only for alipay
-        hideLoading: true,
+onNavigationBarSearchInputConfirmed((e) => {
+  console.log("点击了确定", e.text);
+});
 
-        success: (uploadFileRes) => {
-          console.log(uploadFileRes.data);
-          // alert ok
-          uni.showToast({
-            title: "上传成功",
-            icon: "success",
-            duration: 2000,
-          });
-        },
-        fail: (err) => {
-          console.log(err);
-          // alert fail
-          uni.showToast({
-            title: "上传失败",
-            icon: "none",
-            duration: 2000,
-          });
-        },
-      });
-    },
-  });
-};
+onNavigationBarSearchInputChanged((e) => {
+  console.log("输入框的内容是", e.text);
+});
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
-</style>
+<style></style>
