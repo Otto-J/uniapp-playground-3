e951a63481ce508f7e1af8aae612328196f08740
jixinbao <jixinbao@dcloud.io>
2024-04-15 17:29:06 +0800

    feat: 添加调试代码

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,86 +1,106 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-    <uni-card
-      title="基础卡片"
-      sub-title="副标题"
-      extra="额外信息"
-      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-    >
-      <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
-    </uni-card>
-    <button @click="uploadImage">上传文件</button>
-  </view>
+	<view class="content">
+
+    <div>content</div>
+
+		<!-- #ifdef MP-TOUTIAO -->
+		<view>
+			抖音退款2
+		</view>
+		<pay-button :order-status="orderStatus" :order-id="orderId" @refund="dy_pay_onRefund" 
+		bind:applyrefund="onApplyRefund"
+			@error="dy_pay_error" />
+		<!-- #endif -->
+
+	</view>
 </template>
 
-<script setup lang="ts">
-import { ref } from "vue";
-const title = ref("Hello");
-
-const uploadImage = () => {
-  uni.chooseVideo({
-    sourceType: ["camera", "album"],
-    success: (chooseImageRes) => {
-      const tempFilePaths = chooseImageRes.tempFilePath;
-      // debugger;
-      uni.uploadFile({
-        url: "http://192.168.31.60:3000/upload", //仅为示例，非真实的接口地址
-        filePath: tempFilePaths,
-        name: "file",
-        // only for alipay
-        hideLoading: true,
-
-        success: (uploadFileRes) => {
-          console.log(uploadFileRes.data);
-          // alert ok
-          uni.showToast({
-            title: "上传成功",
-            icon: "success",
-            duration: 2000,
-          });
-        },
-        fail: (err) => {
-          console.log(err);
-          // alert fail
-          uni.showToast({
-            title: "上传失败",
-            icon: "none",
-            duration: 2000,
-          });
-        },
-      });
-    },
-  });
-};
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				orderStatus: 1,
+				orderId: '20240408153004588692'
+			}
+		},
+		onLoad() {
+			// let a = {
+			// 	{
+			// 		[
+			// 			['refund', [
+			// 				['dy_pay_onRefund', ['$event']]
+			// 			]],
+			// 			['applyrefund', [
+			// 				['dy_pay_applyRefund', ['$event']]
+			// 			]],
+			// 			['error', [
+			// 				['dy_pay_error', ['$event']]
+			// 			]]
+			// 		]
+			// 	}
+			// }
+	}, 
+	created() { 
+		this.$scope.onApplyRefund = this.onApplyRefund
+	},
+		methods: {
+			async onApplyRefund(event) {
+				// console.log('dy_pay_applyRefund event= ', event)
+				let extra = {
+					orderId: event.detail
+				}; // 开发者需要透传的参数，可自定义内容
+				const res = new Promise(resolve => {
+					resolve(extra)
+				})
+				console.log(222)
+				return res
+				// return extra
+			},
+			// bind:refund-回调
+			dy_pay_onRefund(event) {
+				console.log(222)
+				console.log('dy_pay_onRefund event=', event)
+				const {
+					status,
+					result
+				} = event.detail;
+				if (status === 'success') {
+					uni.$u.toast('退款成功')
+				}
+			},
+			dy_pay_error(event) {
+				console.log(333,'err',event)
+				// console.log('dy_pay_error event=', event)
+			}
+		}
+	}
 </script>
 
 <style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
 
-.text-area {
-  display: flex;
-  justify-content: center;
-}
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
 
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
-
+debugger;
+const plugin = uni();
+console.log(plugin);
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [plugin],
 });
