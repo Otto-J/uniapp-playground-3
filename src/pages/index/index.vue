fceaa6d3e718336eea262cb7a5a93b520a79cfc3
jixinbao <jixinbao@dcloud.io>
2024-05-14 17:13:53 +0800

    feat: add lark editor

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,86 +1,59 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-    <uni-card
-      title="基础卡片"
-      sub-title="副标题"
-      extra="额外信息"
-      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-    >
-      <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
-    </uni-card>
-    <button @click="uploadImage">上传文件</button>
-  </view>
+ <view>
+  <!-- #ifdef MP-LARK -->
+  <editor
+        style="height:500px"
+        id="editor"
+        :placeholder="data1.placeholder"
+        :contents="data1.contents"
+        :read-only="data1.readOnly"
+        :plugins="data1.plugins"
+        :placeholderStyle="data1.placeholderStyle"
+        @ready="onEditorReady"
+        @mentionselect = "onMentionSelect"
+        @mentionclick = "onMentionClick"
+        @input="onEditorInputValueChange"
+        @insertimage="onInsertImages">
+    </editor>
+
+  <!-- #endif -->
+ </view>
 </template>
 
-<script setup lang="ts">
-import { ref } from "vue";
-const title = ref("Hello");
-
-const uploadImage = () => {
-  uni.chooseVideo({
-    sourceType: ["camera", "album"],
-    success: (chooseImageRes) => {
-      const tempFilePaths = chooseImageRes.tempFilePath;
-      // debugger;
-      uni.uploadFile({
-        url: "http://192.168.31.60:3000/upload", //仅为示例，非真实的接口地址
-        filePath: tempFilePaths,
-        name: "file",
-        // only for alipay
-        hideLoading: true,
-
-        success: (uploadFileRes) => {
-          console.log(uploadFileRes.data);
-          // alert ok
-          uni.showToast({
-            title: "上传成功",
-            icon: "success",
-            duration: 2000,
-          });
+<script lang="ts" setup>
+import  { ref } from 'vue';
+const data1 = ref({
+   placeholder: 'Hello editor!',
+        readOnly: false,
+        contents: {
+            html: `<div id="magicdomid-1_19" class="ace-line align-center heading-h1  locate lineguid-B2nSe8" dir="auto"><span class=" ">飞书</span></div><div id="magicdomid-1_26" class="ace-line blockquote blockquote  locate lineguid-17mqsc" dir="auto"><span class=" ">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 飞书是字节跳动旗下办公平台，整合即时沟通、日历、音视频会议、云文档、云盘、工作台等功能于一体，成就组织和个人，更高效、更愉悦。</span></div><div id="magicdomid-1_27" class="ace-line locate lineguid-6UxLlZ" dir="auto"><span class=" ">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2020年2月24日，字节跳动旗下办公套件飞书宣布，向全国所有企业和组织免费开放，不限规模，不限使用时长，所有用户均可使用飞书全部套件功能。2020年11月18日，飞书在北京举办“2020飞书未来无限大会”。会上，飞书推出全新版本“π”，发布独立App“飞书文档”，并在视频会议、即时沟通等功能上宣布了重大更新。</span></div><div id="magicdomid-1_37" class="ace-line locate lineguid-AWIi9E" dir="auto"><span class=" ">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span class=" backgroundcolor " style="background-color: #FFF362; ">公众评价：小米选用了飞书，目前已经有一段时间了，飞书在信息创建、分享，以及协同办公方面，非常简洁、高效，的确越用越顺手。 ​​​（小米公司创始人、董事长兼CEO 评价）</span></div><div id="magicdomid-1_38" class="ace-line" dir="auto"><br></div><div id="magicdomid-1_34" class="ace-line" dir="auto"><br></div><div id="magicdomid-1_35" class="ace-line image-upload single-line" dir="auto"><span><div contenteditable="false" class="image-container single-elem-cls"><span class="image-wrapper"><span class="point tl n-icon-dragable"></span><span class="point tr n-icon-dragable"></span><span class="point br n-icon-dragable"></span><span class="point bl n-icon-dragable"></span><img src="https://sf3-cn.feishucdn.com/obj/open-platform-opendoc/37338d385a39166169f6bfeaaee5b32f_B6WlUG5Z3o.png" data-faketext=" " data-uuid="LxW0dlVX" class="editor_image" style="width: 88px;height: 88px"></span></div></span></div><div id="magicdomid-1_36" class="ace-line" dir="auto"><br></div>`
         },
-        fail: (err) => {
-          console.log(err);
-          // alert fail
-          uni.showToast({
-            title: "上传失败",
-            icon: "none",
-            duration: 2000,
-          });
-        },
-      });
-    },
-  });
-};
+        plugins: ['attribution', 'mention', 'undo', 'redo','indentRight','indentLeft'],
+        placeholderStyle: {
+            color: '#FFFD00',
+            fontSize:"25px"
+        }
+})
+// const content = ref<
+
+function onEditorReady (res) {
+        console.log('onEditorReady '  + JSON.stringify(res))
+    }
+  function  onEditorInputValueChange(res) {
+        console.log('onEditorInputValueChange '  + JSON.stringify(res))
+    }
+  function  onMentionSelect(res) {
+        console.log('onMentionSelect ' + JSON.stringify(res))
+    }
+   function onMentionClick(res) {
+        console.log('onMentionClick ' + JSON.stringify(res))
+    }
+  function  onInsertImages (res) {
+        console.log('onInsertImages '  + JSON.stringify(res));
+        const images = res.detail.images.map(item => ({
+            ...item,
+            src: item.filePath,
+        }))
+        res.insertImagesCallback({ images });
+    }
 </script>
-
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
-</style>
