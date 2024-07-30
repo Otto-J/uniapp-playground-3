767e44c0412537c95916b497a75e79baed96d4d9
jixinbao <jixinbao@dcloud.io>
2024-07-30 17:14:35 +0800

    feat: 更新 video demo

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,100 +1,35 @@
 <template>
-	<view>
-		<page-head :title="title"></page-head>
-		<view class="uni-padding-wrap uni-common-mt" v-if="showVideo">
-			<view>
-				<video id="myVideo" src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/2minute-demo.mp4"
-					@error="videoErrorCallback" :danmu-list="danmuList" enable-danmu danmu-btn controls
-					poster="https://web-assets.dcloud.net.cn/unidoc/zh/poster.png"></video>
-			</view>
-			<!-- #ifndef MP-ALIPAY || MP-TOUTIAO || MP-KUAISHOU || MP-LARK || MP-JD -->
-			<view class="uni-list uni-common-mt">
-				<view class="uni-list-cell">
-					<view>
-						<view class="uni-label">弹幕内容</view>
-					</view>
-					<view class="uni-list-cell-db">
-						<input v-model="danmuValue" class="uni-input" type="text" placeholder="在此处输入弹幕内容" />
-					</view>
-				</view>
-			</view>
-			<view class="uni-btn-v">
-				<button @click="sendDanmu" class="page-body-button">发送弹幕</button>
-			</view>
-			<!-- #endif -->
-		</view>
-	</view>
+	<div>
+		<video @timeupdate='OnTimeUpdate' id="video1"
+			src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/uni-app-video-courses.mp4"></video>
+		<button @click="onPlay">play/pause</button>
+		<button @click="onSeek">seek 13s</button>
+	</div>
 </template>
-<script>
-	export default {
-		data() {
-			return {
-				title: 'video',
-				src: '',
-				danmuList: [{
-						text: '要显示的文本00',
-						color: '#ff00ff',
-						time: 2
-					},
-					{
-						text: '要显示的文本11',
-						color: '#00ffc9',
-						time: 4
-					},
-					{
-						text: '要显示的文本33',
-						color: '#4700ff',
-						time: 8
-					}
-				],
-				danmuValue: '',
-				showVideo: false
-			}
-		},
-		onReady: function(res) {
-			// #ifndef MP-ALIPAY || MP-TOUTIAO
-			this.videoContext = uni.createVideoContext('myVideo')
-			// #endif
-			// #ifdef APP-PLUS || MP-BAIDU
-			setTimeout(() => {
-				this.showVideo = true
-			}, 350)
-			// #endif
-			// #ifndef APP-PLUS || MP-BAIDU
-			this.showVideo = true
-			// #endif
-		},
-		methods: {
-			sendDanmu: function() {
-				this.videoContext.sendDanmu({
-					text: this.danmuValue,
-					color: this.getRandomColor()
-				});
-				this.danmuValue = '';
-			},
-			videoErrorCallback: function(e) {
-				uni.showModal({
-					content: e.target.errMsg,
-					showCancel: false
-				})
-			},
-			getRandomColor: function() {
-				const rgb = []
-				for (let i = 0; i < 3; ++i) {
-					let color = Math.floor(Math.random() * 256).toString(16)
-					color = color.length == 1 ? '0' + color : color
-					rgb.push(color)
-				}
-				return '#' + rgb.join('')
-			}
+<script lang="ts" setup>
+	import { ref } from 'vue'
+	const videoRef = uni.createVideoContext('video1');
+	console.log(videoRef)
+
+	const isPlay = ref(false)
+
+	function onPlay() {
+		if (!isPlay.value) {
+
+			videoRef.play()
+
+			isPlay.value = true
+		} else {
+			videoRef.pause()
 		}
+	}
+
+	function onSeek() {
+		videoRef.seek(13)
+	}
+	function OnTimeUpdate(e) {
+		console.log('time update', e.detail);
 	}
 </script>
 
-<style>
-	video {
-		width: 690rpx;
-		width: 100%;
-		height: 400px;
-	}
-</style>
+<style></style>
